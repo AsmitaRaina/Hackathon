4a6afe1e754e5d58ed3ffa07a52e88b0a74528ef
SuperChamp234 <beingzainsv@gmail.com>
2023-01-14 03:41:59 +0530

    Added Message Component

---
+++ client/src/components/MessageComponent/MessageComponent.jsx
@@ -0,0 +1,26 @@
+import { Button } from '@mui/material';
+import React from 'react'
+
+const MessageComponent = () => {
+    const path = window.location.pathname;
+    console.log(window.location.pathname);
+    return (
+        <div>
+            <div class="flex">
+                <h1> Subject </h1>
+                <span class="bg-blue-100 text-blue-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-blue-900 dark:text-blue-300">Default</span>
+                <span class="bg-gray-100 text-gray-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded mr-2 dark:bg-gray-700 dark:text-gray-400 border border-gray-500">
+                    <svg aria-hidden="true" class="w-3 h-3 mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-12a1 1 0 10-2 0v4a1 1 0 00.293.707l2.828 2.829a1 1 0 101.415-1.415L11 9.586V6z" clip-rule="evenodd"></path></svg>
+                    3 days ago
+                </span>
+            </div>
+            <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Enim necessitatibus reiciendis vitae quidem, blanditiis vero dicta accusamus corporis. Fugit ducimus magnam, magni aliquid nihil ex. Voluptas necessitatibus repudiandae consequuntur distinctio possimus veritatis quibusdam a in eaque labore, aperiam repellendus cupiditate odit recusandae minus quam! Blanditiis incidunt quo officia nesciunt itaque.
+            </p>
+
+
+        </div>
+    )
+}
+
+export default MessageComponent
