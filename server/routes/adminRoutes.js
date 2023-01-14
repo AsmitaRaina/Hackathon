798e7fe39e2d769c5e7dbe01f352d48f2452cd2d
labeeb-7z <94868003+labeeb-7z@users.noreply.github.com>
2023-01-14 06:27:21 +0530

    blablab

--- server/routes/adminRoutes.js
+++ server/routes/adminRoutes.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const {authController, getAdminProfile, registerAdmin} = require("../controllers/adminController")
+const { authController, getAdminProfile, registerAdmin } = require("../controllers/adminController")
 
 const {protect} = require('../middleware/authMiddleware')
-const Company = require("../models/CompanyModel");
-const Student = require("../models/StudentModel");
+const { protect } = require('../middleware/authMiddleware')
+
 const router = express.Router();
+
+const posts = require('./announcements/postRoutes')
+const replies = require('./announcements/replyRoutes')
+const tags = require('./announcements/tagRoutes')
 
 router.route('/').post(registerAdmin)
 router.route("/login").post(authController);
@@ -15,5 +20,11 @@ router.get('/dashboard', async (req, res) => {
     const companies = await Company.find({}).populate('year.selectedStudents').populate('year.appliedStudents')
     res.send(companies)
 })
+
+
+module.exports = router;
+router.use('/posts/', posts)
+router.use('/replies/', replies)
+router.use('/tags/', tags)
 
 module.exports = router;
